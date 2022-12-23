efe99aad82fc4a61fd9e3f68eeb7edc5f472af1e
EDUELBOT <jvasquezvaaquez@gmail.com>
2022-12-22 18:39:45 -0600

    acabado

---
+++ PoductoInterno.js
@@ -0,0 +1,12 @@
+let numero1 = parseInt(prompt("ingrese un numero"));
+       let numero4 = parseInt(prompt("ingrese un numero"));
+       let numero2 = parseInt(prompt("ingrese un numero"));
+       let numero5 = parseInt(prompt("ingrese un numero"));
+       let numero3 = parseInt(prompt("ingrese un numero"));
+       let numero6 = parseInt(prompt("ingrese un numero"));
+        
+        if (numero1 * numero4 + numero2 * numero5 + numero3 * numero6 = 0){
+            console.log("son ortogonales")
+        }else if (numero1 * numero4 + numero2 * numero5 + numero3 * numero6 < 0){
+            console.log("error")
+        }
